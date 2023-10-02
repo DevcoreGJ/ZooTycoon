979dc54d095a9167abcb7afaa5a2b3f94a155579
devcore_GJ <elliotledson@gmail.com>
2023-10-02 11:33:45 +0100

    dump coded the CAnimals class

--- ZooTycoon.cpp
+++ ZooTycoon.cpp
@@ -1,20 +1,34 @@
-// ZooTycoon.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <string>
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+class CAnimal {
+public:
+    // Constructor
+    CAnimal(int age, std::string name) {
+        this->age = age;
+        this->name = name;
+    }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+    // Pure virtual function to get the species name
+    virtual std::string GetSpeciesName() const = 0;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    // Accessor function to get the name
+    std::string GetName() const {
+        return name;
+    }
+
+    // Accessor function to get the age
+    int GetAge() const {
+        return age;
+    }
+
+    // Virtual destructor
+    virtual ~CAnimal() {
+        std::cout << "Destroying " << GetSpeciesName() << " named " << name << std::endl;
+    }
+
+private:
+    // Member variables
+    int age;
+    std::string name;
+};
